3b0ecc2100f6f6c72dee35783d55fd9cd459fab1
djaus2 <davidjones@sportronics.com.au>
2020-07-11 11:41:43 +1000

    First code push

---
+++ Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MyLib
+{
+    public class Class1
+    {
+        public int Count { get; set; } = 0;
+        public Class1()
+        {
+            Count = 137;
+        }
+
+        public Class1(int count)
+        {
+            Count = count;
+        }
+
+        public void  Inc()
+        {
+            Count++;
+            if (Count > 137)
+                Count = 137;
+        }
+
+        public void Dec()
+        {
+            Count--;
+            if (Count < 0)
+                Count = 0;
+        }
+    }
+}
